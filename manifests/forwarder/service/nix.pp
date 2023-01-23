964a7db7a1b4b6fd5f7f4d8db21ad618ec5c7101
Jamison Maxwell <jamison_maxwell@genpt.com>
2023-01-23 09:35:17 -0500

    got past the stop - now need the start to work

--- manifests/forwarder/service/nix.pp
+++ manifests/forwarder/service/nix.pp
@@ -83,7 +83,7 @@ class splunk::forwarder::service::nix inherits splunk::forwarder::service {
   }
   $accept_tos_command = [
     "${splunk::forwarder::forwarder_homedir}/bin/splunk stop -f &&",
-    "${splunk::forwarder::forwarder_homedir}/bin/splunk start --accept-license --answer-yes &&",
+    "${splunk::forwarder::forwarder_homedir}/bin/splunk start --accept-license --answer-yes --no-prompt &&",
     "${splunk::forwarder::forwarder_homedir}/bin/splunk stop",
   ]
   exec { 'splunk-forwarder-accept-tos':
