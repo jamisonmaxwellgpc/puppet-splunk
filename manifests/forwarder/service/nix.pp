70baa519e9fee5ffc9ea8dc731749bf7414b2b9e
Jamison Maxwell <jamison_maxwell@genpt.com>
2023-01-23 09:21:45 -0500

    trying with the force option

--- manifests/forwarder/service/nix.pp
+++ manifests/forwarder/service/nix.pp
@@ -82,7 +82,7 @@ class splunk::forwarder::service::nix inherits splunk::forwarder::service {
     $accept_tos_subscribe = undef
   }
   $accept_tos_command = [
-    "${splunk::forwarder::forwarder_homedir}/bin/splunk stop &&",
+    "${splunk::forwarder::forwarder_homedir}/bin/splunk stop -f &&",
     "${splunk::forwarder::forwarder_homedir}/bin/splunk start --accept-license --answer-yes &&",
     "${splunk::forwarder::forwarder_homedir}/bin/splunk stop",
   ]
